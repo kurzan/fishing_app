baaaafa477a792883b54f49d275068cc166c161f
Kurzov <kurzanbl4@gmail.com>
2023-09-08 12:06:27 +0300

    Initial app

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import YaMap from 'react-native-yamap';
+
+const Map = () => {
+  return (
+    <YaMap
+      userLocationIcon={{
+        uri: 'https://www.clipartmax.com/png/middle/180-1801760_pin-png.png',
+      }}
+      initialRegion={{
+        lat: 50,
+        lon: 50,
+        zoom: 10,
+        azimuth: 80,
+        tilt: 100,
+      }}
+      style={{flex: 1}}
+    />
+  );
+};
+export default Map;
